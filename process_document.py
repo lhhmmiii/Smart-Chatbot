6a0187c412da93a779e46b061fb64cc3a5bcf1e7
lhhmmiii <lehuuhung30023010@gmail.com>
2024-08-17 09:41:39 +0700

    process table PDF

--- process_document.py
+++ process_document.py
@@ -40,20 +40,51 @@ def split_page_info(body_content, list_separate_page):
 
   return list_page_content_with_html
 
-def extract_info(pdf_path, output_html_path):
-    convert_pdf_to_html(pdf_path, output_html_path)
-    with open(output_html_path, 'r', encoding='utf-8') as html_file:
+def preprocess_table(dfs):
+    new_dfs = []
+    for table in dfs:
+        table = table.applymap(lambda x: x.replace('\r', '\n') if isinstance(x, str) else x)
+        new_dfs.append(table)
+    return new_dfs
+
+
+def extract_table(page_content, table):
+  # Xác định vị trí
+  list_line_in_content = page_content.split('\n')
+  start_pos = list_line_in_content.index(table.columns[0])
+  end_pos = start_pos
+  table_text = table.values.flatten()
+  for i in range(len(list_line_in_content)-1,0,-1):
+    if list_line_in_content[i] in table_text:
+      end_pos = i
+      break
+  # Thay thế
+  del list_line_in_content[start_pos:end_pos + 1]
+  new_content = '\n'.join(list_line_in_content)
+  table_text = table.to_string(index=False)
+  combined_content = f"{new_content}{table_text}"
+  return combined_content
+
+def extract_info(pdf_path, html_file_path):
+    tables = tabula.read_pdf(pdf_path, pages='all', multiple_tables=True)
+    with open(html_file_path, 'r', encoding='utf-8') as html_file:
         soup = BeautifulSoup(html_file, 'html.parser')
     body = soup.body
     list_page_content = []
     list_separate_page = indentify_separate_line(body)
     list_page_content_with_html = split_page_info(body, list_separate_page)
-    for html_page in list_page_content_with_html:
+    for i, html_page in enumerate(list_page_content_with_html):
       content = ''
       for child in html_page.children:
         if child.name:
           text = child.get_text(strip=False)
           if text != '':
             content += text
-      list_page_content.append(content)
+      if i < len(tables):
+        table = tables[0]
+        new_content = extract_table(content, table)
+        list_page_content.append(new_content)
+      else:
+        list_page_content.append(content)
+
     return list_page_content
